c57da6d378e3517c07e4b4649d7e9069c68d2c61
Davi Ribeiro <46770855+odaviribeiro@users.noreply.github.com>
2019-07-01 00:12:28 -0300

    Documentação de Atribuição e Ternários

---
+++ atribuicao-e-ternarios/script.js
@@ -0,0 +1,115 @@
+// O que são variáveis?
+// Variáveis são pequenos valores que são
+// salvos na memória do computador do usuário
+// Não sou o melhor explicando, porém é
+// basicamente isso.
+
+var nome = 'Davi Ribeiro'; // Aqui eu salvei na
+// variável o meu nome, assim eu posso chama-la
+// no meu código novamente caso eu precise.
+
+console.log(`Meu nome é ${nome}.`); // Aqui eu 
+// coloquei uma template sting que são as 
+// crases `` para conseguir passar uma
+// variável dentro do console sem precisar
+// fazer a concatenação para voltar no 
+// meu console a seguinte frase Meu nome é 
+// Davi Ribeiro.
+
+// Podemos utilizar variáveis para fazer somas
+// concatenações, divisões, subtrações,
+// multiplicações, modulação, salvar strings.
+
+// Seguindo esta linha de raciocínio você pode
+// fazer o seguinte.
+
+// Aqui iremos fazer uma soma e retornar no
+// console do browser.
+var x = 5; // Defini o valor que essa variável irá
+// conter que no caso é 5.
+var y = 10; // Defini também o valor que essa
+// variável irá conter que é 10.
+
+var soma = x + y; // Aqui eu criei uma variável
+// que irá conter o resultado das duas variáveis
+// anteriores sendo somadas.
+// Será retornado o valor de 15.
+console.log(soma); // Aqui faz o resultado ser
+// visível no console do browser.
+
+
+// Aqui iremos fazer uma divisão e retornar o valor
+// no browser.
+var x = 10;
+var y = 2;
+
+var divisao = x / y; // A "/" é o sinal de divisão
+// em javascript.
+
+console.log(divisao); // Retornando o valor da
+// variável.
+
+// Aqui iremos fazer uma multiplicação e retornar
+// o valor no browser.
+var x = 2;
+var y = 12;
+
+var multiplicacao = x * y; // Aqui eu defino
+// o que acontecerá com as variáveis que no caso
+// é a multiplicação.
+
+console.log(multiplicacao); // Aqui retorna o
+// resultado da multiplicação de x e y.
+
+
+// Aqui iremos fazer uma subtração e retornar
+// o resultado no browser.
+var x = 50;
+var y = 25;
+
+var subtracao = x - y; // Aqui eu faço o x ser
+// subtraído por y.
+
+console.log(subtracao); // Retorna o resultado
+// da variável no browser.
+
+// Chegamos na parte de contatenação, porém
+// tenho uma observação, um número só
+// concatena com outro se o primeiro número
+// ou o segundo for um string ou seja esteja
+// dentro de '5' por exemplo.
+var x = '5';
+var y = 1;
+
+var concatenacao = x + y; // Aqui acontece a
+// concatenacao que é unir os dois números
+// ao contrário da soma.
+
+console.log(concatenacao); // Aqui nós vamos
+// retornar o resultado dessa concatenacao
+// que será 51.
+
+// Aqui nós irémos fazer a modulação
+var x = 15;
+var y = 10;
+
+var modulacao = x % y; // O simbolo utilizado no
+// javascript de modulação é o %.
+
+console.log(modulacao); // Retornamos o resultado
+// da modulação.
+
+// Bom não sou o melhor "programador" javascript
+// porém tenho o foco em ajudar a comunidade
+// escrevi esta documentação para iniciantes
+// entenderem um pouco mais da linguagem
+// e principalmente as variáveis.
+
+// Existem mais variáveis, mas por enquanto
+// foquem apenas nessa e depois tente mais.
+
+let primeiravariavel = '';
+const constante = '';
+var variavel = '';
+
+// Essas são todas as variáveis.
